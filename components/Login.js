35208b00a7fd5959935e51c617ada182bcb9b858
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-28 17:18:38 +0900

    import graph from twitter

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 const Login = () => {
   return (
     <div className="bg-black relative text-white">
       <h1>Login</h1>
+      <div>
+        {/* Logo */}
+
+        {/* login button */}
+      </div>
+      <div className="w-full h-screen">
+        <Image
+          src="https://pbs.twimg.com/media/FHDbjjMaQAIQfEL?format=jpg&name=large"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
     </div>
   );
 };
