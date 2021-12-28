4faefd28bacd32189218816f34a6e0831e84d62c
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-28 17:39:00 +0900

    style login icon and button

--- components/Login.js
+++ components/Login.js
@@ -2,12 +2,22 @@ import Image from "next/image";
 
 const Login = () => {
   return (
-    <div className="bg-black relative text-white">
+    <div className="bg-black relative">
       <h1>Login</h1>
-      <div>
-        {/* Logo */}
-
-        {/* login button */}
+      <div
+        className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center
+      space-y-4"
+      >
+        <Image
+          className="object-cover rounded-2xl"
+          src="https://pbs.twimg.com/media/FHIKUtlaAAEukIA?format=jpg&name=large"
+          alt="Metaverse Challenge"
+          width={200}
+          height={290}
+        />
+        <button className="bg-green-400 rounded-lg p-5 font-bold animate-pulse">
+          Login to the Metaverse
+        </button>
       </div>
       <div className="w-full h-screen">
         <Image
