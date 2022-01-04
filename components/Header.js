870c3eadf5cfec78927eb0d1a4b767ad8d971a42
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2022-01-04 23:22:23 +0900

    Add Header component

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>I am a Header</h1>
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
 import Login from "../components/Login";
 
 export default function Home() {
@@ -8,12 +9,16 @@ export default function Home() {
   if (!isAuthenticated) return <Login />;
 
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
       <Head>
         <title>Metaverse Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to the APP</h1>
+      <div className="max-w-screen-2xl mx-auto">
+        {/* Header */}
+        <Header />
+        {/* Messages */}
+      </div>
       <button onClick={logout}>Logout</button>
     </div>
   );
