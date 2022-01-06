a7f0977569effa3468499f0b88b7d70bead80e8a
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2022-01-06 22:29:10 +0900

    style header

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,11 @@ const Header = () => {
   const { user } = useMoralis();
 
   return (
-    <div className="text-pink-500">
-      <div className="">
+    <div
+      className="sticky top-0 p-5 z-50 bg-black shadow-sm text-pink-500
+    border-b-2 border-pink-700"
+    >
+      <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
         <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
           <Image
             layout="fill"
@@ -17,7 +20,7 @@ const Header = () => {
             src="https://pbs.twimg.com/media/FIGZXB2akAAZvWN?format=jpg&name=900x900"
           />
         </div>
-        <div>
+        <div className="col-span-4 text-left lg:text-center">
           {/* Avatar */}
           <div className="relative h-48 w-48 lg:mx-auto border-pink-500 border-8 rounded-full">
             <Avatar logoutOnPress />
